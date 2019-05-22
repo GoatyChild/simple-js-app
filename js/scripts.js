4f00e82b6f13790b92a6186ea177582db91b87fe
GoatyChild <GoatyChild@users.noreply.github.com>
2019-05-22 23:53:24 +0200

    added and removed content

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
 alert('You clicked the button!')
+
+var simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+var size = 100;
+var doubleSize = size * 2;
+document.write(doubleSize);
+
+var size = 100;
+var doubleSize = size * 2;
+var minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
