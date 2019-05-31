cdad0c6588eb55850935c938ababb091b5e913e0
GoatyChild <GoatyChild@users.noreply.github.com>
2019-05-31 11:04:51 +0200

    added js-app:
    created objects (bulbasaur, charmander, squirtle)
    
    created objects
    
    created for loop
    
    added conditional to for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,52 @@
-var repository = [bulbasaur, charmander, squirtle]
-
-var bulbasaur = {
+    var bulbasaur = {
     
-    name: bulbasaur,
+    name: 'bulbasaur',
     height: 7,
     type: 'grass'
-}
 
-var charmander = {
+    }
     
-    name: charmander,
+    var charmander = {
+    
+    name: 'charmander',
     height: 10,
     type: 'fire'
-}
-
-var squirtle = {
     
-    name: squirtle,
+    }
+    
+    var squirtle = {
+    
+    name: 'squirtle',
     height: 11,
     type: 'water'
-}
+    
+    }
+    var pokemonRepository = [bulbasaur, charmander, squirtle] 
+
+        
+    for (var i = 0; i < pokemonRepository.length; i++) {
+        
+        console.log(pokemonRepository[i].name);
+        
+        document.write('<p>' + pokemonRepository[i].name + ' height:' + pokemonRepository[i].height + ' type:' + pokemonRepository[i].type + '</p>')
+        
+        if (pokemonRepository[i].height >= 3 ) {
+            
+            console.log('Wow that is big!');
+            
+            document.write('Wow that is big!');
+            
+        }
+
+    }
+
+	
+
+
+
+    
+
+    
+    
+    
+    
