d5c52195a637b01ff7216bb2636e8a79b7c971c0
GoatyChild <GoatyChild@users.noreply.github.com>
2019-06-14 00:02:10 +0200

    added forEach loop to pokemonRepository

--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,33 @@
     var bulbasaur = {
-    
+
     name: 'bulbasaur',
     height: 7,
     type: 'grass'
 
     }
-    
+
     var charmander = {
-    
+
     name: 'charmander',
     height: 10,
     type: 'fire'
-    
+
     }
-    
+
     var squirtle = {
-    
+
     name: 'squirtle',
     height: 11,
     type: 'water'
-    
-    }
-    var pokemonRepository = [bulbasaur, charmander, squirtle] 
-
-        
-    for (var i = 0; i < pokemonRepository.length; i++) {
-        
-        console.log(pokemonRepository[i].name);
-        
-        document.write('<p>' + pokemonRepository[i].name + ' height:' + pokemonRepository[i].height + ' type:' + pokemonRepository[i].type + '</p>')
-        
-        if (pokemonRepository[i].height >= 3 ) {
-            
-            console.log('Wow that is big!');
-            
-            document.write('Wow that is big!');
-            
-        }
 
     }
 
-    function loopBlockFunction(currentPokemon){
-        
-        console.log(currentPokemon);
-        document.write(currentPokemon);
-    }
+    var pokemonRepository = [bulbasaur, charmander, squirtle]
 
-    pokemonRepository.forEach(loopBlockFunction);
+    pokemonRepository.forEach(function(currentPokemon){
 
-	
-   
-    
+      console.log(currentPokemon);
+      document.write('<p>' + ': ' + currentPokemon + '</p>');
+      
 
-    
-    
-    
-    
+    });
